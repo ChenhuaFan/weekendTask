ed3b1f148b4e3b9957eb821721d1bb5795e3ba41
newliar <1107325513@qq.com>
2018-04-01 01:27:21 +0800

    上传order和user测试脚本和文件

--- src/test/unit_test/order_test.c
+++ src/test/unit_test/order_test.c
@@ -5,22 +5,24 @@
 int main(int args, char *argv[]) {
     order_t_p order_list = create_order();
     order_t_p order_index = NULL;
-    /*
-    for(int i = 1; i<6;i++) {
-        order_index = insert_new_order(order_list);
-        order_index->user_id = i;
-        order_index->ticket_id=i*3;
-        order_index->amount=i*5;
-        order_index->type=i*7;
-        order_index->status=i*9;
-        order_index->code=i*11;
-    }
-    order_write(order_list);*/
+    
+    // for(int i = 1; i<6;i++) {
+    //     order_index = insert_new_order(order_list);
+    //     order_index->user_id = i;
+    //     order_index->ticket_id=i*3;
+    //     order_index->amount=i*5;
+    //     order_index->type=i*7;
+    //     order_index->status=i*9;
+    //     order_index->code=i*11;
+    // }
+    // order_write(order_list);
     
     order_read(order_list);
-    printf("%d\n",order_list->ticket_id);
-    printf("%d\n",order_list->next->next->next->next->ticket_id);
-    // printf("%d\n",order_list->next->next->ticket_id);
-
+    
+    order_t_p p=order_list->next;  
+    while(p!=NULL){  
+        printf("%d %d %d %d %d %d\n",p->user_id, p->ticket_id, p->amount ,p->type ,p->status ,p->code);  
+        p=p->next;  
+    }  
     return 0;
 }
