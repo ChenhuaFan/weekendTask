73e6f250f26aef3558f22ed8da2ef84c6be1497b
ChenhuaFan <805901483@qq.com>
2018-03-31 15:50:50 +0800

    unitest

--- src/main.c
+++ src/main.c
@@ -2,9 +2,21 @@
 #include "./core/data/ticket.c"
 #include "./core/data/order.c"
 
+void init()
+{
+    ticket_t_p tickets = create_tickets();
+    ticket_t_p t_index = NULL;
+    order_t_p orders = create_order();
+    tickets->id = 1;
+    orders->ticket_id = 1;
+    t_index = insert_new_ticket(tickets);
+    t_index->id = 2;
+    printf("t: %d; t->n: %d | o: %d\n", tickets->id, tickets->next->id, orders->ticket_id);
+}
+
 int main(int argc, char* argv[])
 {
     printf("hello world!");
-    
+    init();
     return 0;
 }
