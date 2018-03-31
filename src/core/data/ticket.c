efd62c76cbc810e88d0b91e7916e89ef88a8eb96
ChenhuaFan <805901483@qq.com>
2018-03-31 13:35:33 +0800

    彩票操作实现

--- src/core/data/ticket.c
+++ src/core/data/ticket.c
@@ -4,9 +4,73 @@
 
 #include "ticket.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 // 创建新的彩票链表
 ticket_t_p create_tickets(void)
 {
-    
+    ticket_t_p temp_node = NULL;
+    temp_node = (ticket_t_p)malloc(sizeof(ticket_t));  
+    if (temp_node == NULL)
+    {  
+        // 返回错误code（在其他的资源文件中定义）
+    }
+    else
+    {
+        temp_node->next = NULL;
+    }
+    return temp_node;
+}
+
+ticket_t_p get_ticket_by_id(ticket_t_p *tickets, id_t id);
+{
+    while(tickets->next != NULL)
+    {
+        if(tickets->id == id) 
+        {
+            return tickets;
+        } 
+        else 
+        {
+            // 返回错误code（在其他的资源文件中定义）
+        }
+        tickets = tickets->next;
+    }
+}
+
+id_t delete_ticket_by_id(ticket_t_p *tickets, id_t id)
+{
+    ticket_t_p temp_node = NULL;
+    while(tickets->next != NULL)
+    {
+        if(tickets->next->id == id)
+        {
+            // free next指针指向的节点
+            temp_node = tickets->next->next;
+            free(tickets->next);
+            tickets->next = temp_node;
+        }
+        tickets = tickets->next;
+    }
+    // 返回错误code
+}
+
+ticket_t_p insert_new_ticket(ticket_t_p *tickets)
+{
+    while(1)
+    {
+        if(tickets->next == NULL)
+        {
+            tickets->next = (ticket_t_p)malloc(sizeof(ticket_t));  
+            if (tickets->next == NULL)
+            {  
+                // 返回错误code（在其他的资源文件中定义）
+            }
+            else
+            {
+                tickets->next = NULL;
+            }
+            return tickets->next;
+        }
+    }
 }
