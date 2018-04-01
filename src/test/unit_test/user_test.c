4458fe55dbd7bc487435b78e3f091643df9cf90a
newliar <1107325513@qq.com>
2018-04-01 08:37:07 +0800

    user读写功能测试

--- src/test/unit_test/user_test.c
+++ src/test/unit_test/user_test.c
@@ -12,14 +12,14 @@ int main(int argc, char *argv[]) {
     
 
     //新添用户
-    for(int i = 1; i <= 10; i++) {
-        user_t_p new_user=insert_new_user(index);
-        new_user->id = i;
-        new_user->phone = i*123;
-        new_user->pw = i*456;
-        new_user->money = i*100;
-        index = new_user;
-    }
+    // for(int i = 1; i <= 10; i++) {
+    //     user_t_p new_user=insert_new_user(index);
+    //     new_user->id = i;
+    //     new_user->phone = i*123;
+    //     new_user->pw = i*456;
+    //     new_user->money = i*100;
+    //     index = new_user;
+    // }
     // printf("%d\n",user_list_node->id);
     // printf("%d\n",user_list_node->next->id);
     // printf("%d\n",user_list_node->next->next->id);
@@ -33,14 +33,23 @@ int main(int argc, char *argv[]) {
 
     //删除用户
     
-    key_t deleted_id = delete_user_by_id(user_list_node,3);
-    printf("%d\n",user_list_node->next->next->next->money);
-    user_write(user_list_node);
+    // key_t deleted_id = delete_user_by_id(user_list_node,3);
+    // printf("%d\n",user_list_node->next->next->next->money);
+
+
+    // user_write(user_list_node);
+    // user_t_p p=user_list_node->next;  
+    // while(p!=NULL){  
+    //     printf("%d %d %d %d\n",p->id, p->phone, p->pw ,p->money);  
+    //     p=p->next;  
+    // }  
+
+
+    user_read(user_list_node);
     user_t_p p=user_list_node->next;  
     while(p!=NULL){  
         printf("%d %d %d %d\n",p->id, p->phone, p->pw ,p->money);  
         p=p->next;  
     }  
-
     return 0;
 }
